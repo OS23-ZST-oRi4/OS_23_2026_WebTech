954a1d0e7ce947373ac34b231f77ebbb5aca4195
OS23-ZST-oRi4 <oskar.sikora@zst.tarnow.pl>
2026-09-10 14:27:59 +0200

    Modyfikacja App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,73 @@ import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  
+
+  const app = {
+    name: "WebTech",
+    version: "1.0",
+    author: "Oskar Sikora",
+    technologiesCount: 3
+  };
+
+  const technology = {
+    name: "React",
+    category: "Frontend",
+    hours: 30,
+    active: true
+  };
+
+  const student = {
+    name: "Oskar",
+    surname: "Sikora",
+    className: "4P",
+    specialization: "technik programista"
+  };
+
+  const course = {
+    name: "React/JSX",
+    teacher: "Paul Friday",
+    hours: 24,
+    completed: true
+  };
+
   return (
-    <>
+    <div>
 
+      <h1>{app.name}</h1>
 
+      <p>Wersja: {app.version}</p>
 
-      
-    </>
-  )
+      <p>Autor: {app.author}</p>
+
+      <p>
+        Liczba technologii: {app.technologiesCount}
+      </p>
+
+      <h1>{technology.name}</h1>
+
+      <p>Kategoria: {technology.category}</p>
+
+      <p>Liczba godzin: {technology.hours}</p>
+
+      <h1>Uczeń: {student.name} {student.surname}</h1>
+
+      <p>Klasa: {student.className}</p>
+
+      <p>Kierunek {student.specialization}</p>
+
+      <section className='courseSection'>
+        <br></br>
+        <h2>Nazwa kursu: {course.name}</h2>
+
+        <p>Nauczyciel kursu: {course.teacher}, czas trwania: {course.hours} godziny</p>
+
+        <p>Status ukończenia: {course.completed}</p>
+
+        <p>Wynik końcowy: {Math.floor(Math.random() * 101)}%</p>
+      </section>
+    </div>
+  );
+
 }
 
-export default App
+export default App;
